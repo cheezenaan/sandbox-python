8c812cc6c4f00648247129b26a79e727a23e2964
cheezenaan <cheezenaan@gmail.com>
2024-07-26 16:51:30 +0900

    neetcode: Remove unnecessary initial condition

--- src/neetcode/01_array/217_contains_duplicate_1.py
+++ src/neetcode/01_array/217_contains_duplicate_1.py
@@ -1,12 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        if not nums:
-            return False
-
         nums = sorted(nums)
-        prev = nums[0]
+        prev = None
 
-        for n in nums[1:]:
+        for n in nums:
             if n == prev:
                 return True
             else:
